fb947fb5f73144f383d7c3f322cf5911aebed7c4
Usulidin <67050975+Umarjonov@users.noreply.github.com>
2023-07-07 09:17:28 +0500

    nuxt3 install

---
+++ pages/contact.vue
@@ -0,0 +1,7 @@
+<template>
+  <div>
+    This is the Contact page
+  </div>
+</template>
+<script setup lang="ts">
+</script>
---
+++ pages/posts/[id].vue
@@ -0,0 +1,8 @@
+
+<template>
+  <div>
+    This is the posts id route: {{ $route.params.id }} page
+  </div>
+</template>
+<script setup lang="ts">
+</script>
